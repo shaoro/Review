52ce8264aa87248518fcf05c815dd5b00d1623a8
shaoro <52579116+shaoro@users.noreply.github.com>
2019-08-09 23:21:47 -0400

    Add files via upload

---
+++ assets/data/busScheduleInfo.ts
@@ -0,0 +1,6 @@
+import { BusSchedule } from '../../app/BusSchedule';
+
+export const BUSINFO: BusSchedule[] = [
+  {route: "Warden to Midland", leave: "9:30am", arrived:"10:30am"},
+  {route: "Oakville to Toronto", leave: "11:30am", arrived:"12:30pm"}
+]
